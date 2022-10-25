9f123a6f626ec1b930f47cb69fc8c7469004b379
aspray0 <andrew.d.spray@gmail.com>
2022-10-25 15:42:07 -0700

    Make getComputerChoice function from pseudocode
    Converted the pseudocode for the getComputerChoice function to real code
    computerSelection is currently only defined in this function's scope,
    as it may only be necessary when the function is called. This may
    change in future iterations depending on the necessity of the
    computerSelection variable in other scopes.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // script.js
 
 // make function getComputerChoice
+function getComputerChoice() {
+  
 // create var randomNumber
 // generate random number between 0 and 1
 // put number in randomNumber
+let randomNumber = Math.random();
+
 // create var computerSelection
-// if randomNumber > 0.66
-//   put 'rock' in computerSelection
-//   return computerSelection
-// else if randomNumber > 0.33
-//   put 'paper' in computerSelection
-//   return computerSelection
-// else
-//   put 'scissors' in computerSelection
-//   return computerSelection
+let computerSelection = '';
+
+if (randomNumber > 0.66) {        // if randomNumber > 0.66
+  computerSelection = 'Rock';     //   put 'Rock' in computerSelection
+  return computerSelection;       //   return computerSelection
+} else if (randomNumber > 0.33) { // else if randomNumber > 0.33
+  computerSelection = 'Paper';    //   put 'Paper' in computerSelection
+  return computerSelection;       //   return computerSelection
+} else {                          // else
+  computerSelection = 'Scissors'; //   put 'Scissors' in computerSelection
+  return computerSelection;       //   return computerSelection
+}
+}
