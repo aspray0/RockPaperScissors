9f3d4b8875210fd97e5f08b7699cd2a94753e313
aspray0 <andrew.d.spray@gmail.com>
2022-10-25 15:28:34 -0700

    Enter pseudocode for computerChoice() in comments
    Set up the basic pseudocode framework for the function computerChoice().

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// make function getComputerChoice
+// create var randomNumber
+// generate random number between 0 and 1
+// put number in randomNumber
+// create var computerSelection
+// if randomNumber > 0.66
+//   put 'rock' in computerSelection
+//   return computerSelection
+// else if randomNumber > 0.33
+//   put 'paper' in computerSelection
+//   return computerSelection
+// else
+//   put 'scissors' in computerSelection
+//   return computerSelection
