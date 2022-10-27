a1f072185a16d06512f291b9451209633dca06cf
aspray0 <andrew.d.spray@gmail.com>
2022-10-27 11:45:23 -0700

    Convert playRound pseudocode to code

--- script.js
+++ script.js
@@ -24,20 +24,35 @@ if (randomNumber > 0.66) {        // if randomNumber > 0.66
 }
 
 // make function playRound
+function playRound(playerSelection, computerSelection) {
+
 // set playerSelection to lower case
+playerSelection = playerSelection.toLowerCase();
+
 // set computerSelection to lower case
-// test condition for truthy-ness
-// condition playerSelection same as computerSelection
-//   return string saying it's a tie
-// condition player rock computer paper
-//   return string saying you lose
-// condition player rock computer scissors
-//   return string saying you win
-// condition player paper computer rock
-//   return string saying you win
-// condition player paper computer scissors
-//   return string saying you lose
-// condition player scissors computer rock
-//   return string saying you lose
-// condition player scissors computer paper
-//   return string saying you win
+computerSelection = computerSelection.toLowerCase();
+
+switch (true) {// switch test condition for truthy-ness
+  case (playerSelection === computerSelection):// condition playerSelection same as computerSelection
+    return `It's a tie, ${playerSelection} versus ${computerSelection}!`;//   return string saying it's a tie
+    break;//   break
+  case (playerSelection === 'rock' && computerSelection === 'paper'):// condition player rock computer paper
+    return `You lose, ${computerSelection} beats ${playerSelection}!`;//   return string saying you lose
+    break;//   break
+  case (playerSelection === 'rock' && computerSelection === 'scissors'):// condition player rock computer scissors
+    return `You win, ${playerSelection} beats ${computerSelection}!`; //   return string saying you win
+    break;//   break
+  case (playerSelection === 'paper' && computerSelection === 'rock'):// condition player paper computer rock
+    return `You win, ${playerSelection} beats ${computerSelection}!`;//   return string saying you win
+    break;//   break
+  case (playerSelection === 'paper' && computerSelection === 'scissors'):// condition player paper computer scissors
+    return `You lose, ${computerSelection} beats ${playerSelection}!`;//   return string saying you lose
+    break;//   break
+  case (playerSelection === 'scissors' && computerSelection === 'rock'):// condition player scissors computer rock
+    return `You lose, ${computerSelection} beats ${playerSelection}!`;//   return string saying you lose
+    break;//   break
+  case (playerSelection === 'scissors' && computerSelection === 'paper'):// condition player scissors computer paper
+    return `You win, ${playerSelection} beats ${computerSelection}!`;//   return string saying you win
+    break;//   break
+}
+}
